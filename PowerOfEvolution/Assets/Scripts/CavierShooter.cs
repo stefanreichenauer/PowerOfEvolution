ee1fa77b9a3c27bce34370527b951047877d3a39
stefanreichenauer <stefan-reichenauer@gmx.at>
2017-09-29 20:32:11 +0200

    Folder Structure

---
+++ PowerOfEvolution/Assets/Scripts/CavierShooter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CavierShooter : MonoBehaviour {
+
+    public GameObject cavierPrefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            GameObject cavierBullet = Instantiate(cavierPrefab, gameObject.transform);
+            cavierBullet.GetComponent<Rigidbody>().AddForce(gameObject.transform.forward);
+
+        }
+		
+	}
+}
