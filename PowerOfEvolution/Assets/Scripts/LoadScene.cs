0c599b7d54eac2c181753824bbfe2dd4092b8f41
Tomo <32360115+turekt@users.noreply.github.com>
2017-09-30 18:17:41 +0200

    Merge

---
+++ PowerOfEvolution/Assets/Scripts/LoadScene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour {
+
+	public void LoadSceneByIndex(int scene) {
+		SceneManager.LoadScene(scene);
+	}
+
+	public void OkOnClick() {
+		Canvas canvas = GetComponentInParent<Canvas> ();
+		canvas.enabled = !canvas.enabled;
+	}
+}
---
+++ PowerOfEvolution/Assets/Scripts/PauseMenuController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuController : MonoBehaviour {
+
+	private Canvas pauseMenuCanvas;
+
+	// Use this for initialization
+	void Start () {
+		pauseMenuCanvas = GetComponentInParent<Canvas> ();
+		pauseMenuCanvas.enabled = false;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp(KeyCode.Escape) && pauseMenuCanvas != null) {
+			pauseMenuCanvas.enabled = !pauseMenuCanvas.enabled;
+		}
+		Time.timeScale = pauseMenuCanvas.enabled ? 0.0f : 1.0f;
+	}
+
+	public void ContinueOnClick() {
+		if (pauseMenuCanvas != null && pauseMenuCanvas.enabled) {
+			pauseMenuCanvas.enabled = false;
+		}
+	}
+}
