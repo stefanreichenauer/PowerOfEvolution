8dd39d0d20bb1f7a26582a85901fa82bc669332c
Tomo <32360115+turekt@users.noreply.github.com>
2017-09-30 10:38:20 +0200

    Button handler

---
+++ PowerOfEvolution/Assets/Scripts/ButtonController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour {
+
+	public void OnClick(string skillName) {
+		// change the model
+		// take the xp points from the player
+	}
+}
