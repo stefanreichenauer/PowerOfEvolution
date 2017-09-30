7cefa8b10bdf2ed4dccb746ef1241ffecf5feb81
Tomo <32360115+turekt@users.noreply.github.com>
2017-09-30 10:01:31 +0200

    Skill tree canvas
    Added script for showing and hiding the skill tree canvas by pressing K

---
+++ PowerOfEvolution/Assets/Scripts/ShowHideSkillTree.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowHideSkillTree : MonoBehaviour {
+
+	private Canvas skillTreeCanvas;
+
+	// Use this for initialization
+	void Start () {
+		skillTreeCanvas = GetComponentInParent<Canvas> ();
+		skillTreeCanvas.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp(KeyCode.K) && skillTreeCanvas != null) {
+			skillTreeCanvas.enabled = !skillTreeCanvas.enabled;
+		}
+	}
+}
