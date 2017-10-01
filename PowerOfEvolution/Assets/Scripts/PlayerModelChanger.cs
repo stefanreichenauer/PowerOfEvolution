4a957597eb0ca3050409e1bd92d6aff2c4df6d7c
stefanreichenauer <stefan-reichenauer@gmx.at>
2017-10-01 13:12:06 +0200

    Added all fish models

--- PowerOfEvolution/Assets/Scripts/PlayerModelChanger.cs
+++ PowerOfEvolution/Assets/Scripts/PlayerModelChanger.cs
@@ -7,6 +7,9 @@ public class PlayerModelChanger : MonoBehaviour {
     public GameObject fish_swimming;
     public GameObject fish_legs;
     public GameObject fish_legs_horns;
+    public GameObject fish_legs_hand;
+    public GameObject fish_legs_horns_hand;
+    public GameObject fish_legs_horns_hand_spikes;
 
     GameObject activeModel;
 
@@ -23,11 +26,29 @@ public class PlayerModelChanger : MonoBehaviour {
 
     public void changeModel(bool legs, bool horns, bool hands, bool spikes)
     {
-        if(legs && horns)
+        if (legs && horns && hands && spikes)
+        {
+            activeModel.SetActive(false);
+            fish_legs_horns_hand_spikes.SetActive(true);
+            activeModel = fish_legs_horns_hand_spikes;
+        }
+        else if (legs && horns && hands)
+        {
+            activeModel.SetActive(false);
+            fish_legs_horns_hand.SetActive(true);
+            activeModel = fish_legs_horns_hand;
+        }
+        if (legs && horns)
         {
             activeModel.SetActive(false);
             fish_legs_horns.SetActive(true);
             activeModel = fish_legs_horns;
+        }
+        if (legs && hands)
+        {
+            activeModel.SetActive(false);
+            fish_legs_hand.SetActive(true);
+            activeModel = fish_legs_hand;
         }
         else if (legs)
         {
